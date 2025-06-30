7c255e6c5f73e79aefd05f6417fb2e95cf4fc2f5
hxjini <167085275+hxjini@users.noreply.github.com>
2025-06-30 14:41:02 +0900

    Update Sub4DTO.java

--- HRD_03/src/main/java/exam/Sub4DTO.java
+++ HRD_03/src/main/java/exam/Sub4DTO.java
@@ -1,5 +1,33 @@
 package exam;
 
 public class Sub4DTO {
-
-}
+	private String teacher_code, class_name, teacher_name;
+	private int total;
+	
+	public String getTeacher_code() {
+		return teacher_code;
+	}
+	public void setTeacher_code(String teacher_code) {
+		this.teacher_code = teacher_code;
+	}
+	public String getClass_name() {
+		return class_name;
+	}
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+	public String getTeacher_name() {
+		return teacher_name;
+	}
+	public void setTeacher_name(String teacher_name) {
+		this.teacher_name = teacher_name;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	
+}//class
